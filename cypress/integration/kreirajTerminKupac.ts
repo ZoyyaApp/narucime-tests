32093e67d8e46098b5f0d00f9c82b0f553e94e9b
FilipRukavina <fil.rukavina@gmail.com>
2020-11-15 16:36:36 +0100

    click bug, tempFixv2 za 'while'

--- cypress/integration/kreirajTerminKupac.ts
+++ cypress/integration/kreirajTerminKupac.ts
@@ -30,17 +30,16 @@ describe("User can login and crate an appointment", () => {
     cy.getWaitClick("[data-cy=button_nextStep]", 1000);
     cy.getWaitClick("[data-cy=employee_asd]", 1000); //"[data-cy=employee_null]"
 
-        
-    if(cy.get(".styles__WizardNoTimeTitle-vntfsx-63")){ // while ne radi
+    // temp quickFix; los, malo bolji od dosadasnjeg
+    if(cy.get(".styles__WizardNoTimeTitle-vntfsx-63").should("exist")){ // while ne radi
         cy.get("[data-cy=button_undefined]")
             .wait(1000)  
             .click();
-    }
-
-    if(cy.get(".styles__WizardNoTimeTitle-vntfsx-63")){ // while workaround https://github.com/cypress-io/cypress/issues/8100
-        cy.get("[data-cy=button_undefined]") // ? the state of the DOM is immutable ?
-            .wait(1000)  
-            .click();
+        if(!cy.get(".styles__WizardNoTimeTitle-vntfsx-63").should("not.exist")){ // while workaround https://github.com/cypress-io/cypress/issues/8100
+            cy.get("[data-cy=button_undefined]") // ? the state of the DOM is immutable ?
+                .wait(1000)  
+                .click();
+        }
     }
 
     cy.get(".styles__WizardTimesWrapper-vntfsx-65 > :nth-child(1)").click();
@@ -56,10 +55,11 @@ describe("User can login and crate an appointment", () => {
     // provjera pop-up-a?
 
     // uspjesno kreiran termin
-
-    cy.get("[data-cy=button_nextStep]")
-        //.wait(1000)
-        //.click(); 
+    
+    //cy.get("[data-cy=button_nextStep]")
+    cy.get(".style__ButtonLabel-sc-1yo6tdd-1") 
+        .wait(5000)
+        .click(); 
 
     // click ne radi?
 
