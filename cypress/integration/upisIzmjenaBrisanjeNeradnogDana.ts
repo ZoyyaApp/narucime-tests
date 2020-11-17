fcb11295516f5c9719459b0caae2e0b8e5c1934b
FilipRukavina <fil.rukavina@gmail.com>
2020-11-17 23:03:09 +0100

    dodavanje testa za neradni dan

---
+++ cypress/integration/upisIzmjenaBrisanjeNeradnogDana.ts
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+export const module = 1;
+
+describe("Owner can login and crate an appointment / check existing", () => {
+
+  it("Fill the login form and submit with enter", () => {
+    cy.visit("/login");
+
+    cy.get("[data-cy=button_email]").click();
+
+    cy.getInput("email")
+        .type(Cypress.env("userName"))
+        .should("have.value", Cypress.env("userName")); // userName -> email
+    
+    cy.getInput("password")
+        .type(Cypress.env("password"))
+        .should("have.value", Cypress.env("password"));
+
+    cy.getButton("submit").click();
+
+    cy.location('pathname').should('match', /\/calendar\/day\/*$/)
+
+    cy.getWaitClick('[data-intercom-target="Sidebar-Settings"]', 1000);
+
+    cy.location('pathname').should('include', '/settings/organization/');
+
+    cy.getWaitClick('[href="/bnenl/115/settings/organization/locations/115/holidays"]', 1000);
+    // problem identifiera'
+
+  });
+
+});
