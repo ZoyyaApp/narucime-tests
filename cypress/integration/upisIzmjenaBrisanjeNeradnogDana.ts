f1d7370c740f4c4a4b1befce51d66b788a3b1203
FilipRukavina <fil.rukavina@gmail.com>
2020-11-22 17:48:01 +0100

    brisanje examples foldera, spremanje primjera u upisIzmjenaBrisanjeNeradnogDana.ts

--- cypress/integration/upisIzmjenaBrisanjeNeradnogDana.ts
+++ cypress/integration/upisIzmjenaBrisanjeNeradnogDana.ts
@@ -21,6 +21,10 @@ describe("Owner can login and crate an appointment / check existing", () => {
 
     cy.location('pathname').should('match', /\/calendar\/day\/*$/)
 
+    /*cy.location().should((loc) => {
+      expect(loc.pathname).to.contains("/calendar/day");
+    });*/
+
     cy.getWaitClick('[data-intercom-target="Sidebar-Settings"]', 1000);
 
     cy.location('pathname').should('include', '/settings/organization/');
