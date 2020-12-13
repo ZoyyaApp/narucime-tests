223e039ec0d764f4e7f64b7c7f1a7b7034838e3e
FilipRukavina <fil.rukavina@gmail.com>
2020-12-13 19:37:04 +0100

    fix resizeObserver problema

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,3 +19,11 @@ import "cypress-pipe";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/
+Cypress.on('uncaught:exception', (err) => {
+    /* returning false here prevents Cypress from failing the test */
+    if (resizeObserverLoopErrRe.test(err.message)) {
+        return false
+    }
+})
