71101d77234af9c96abdcb7d29d93375a4f4a569
FilipRukavina <fil.rukavina@gmail.com>
2020-12-25 22:42:00 +0100

    new bug test

---
+++ cypress/integration/bugs/reportsClick.ts
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+export const module = 1;
+
+describe("Owner can login and edit branch data", () => {
+
+  it("Login", () => {
+    
+    cy.visit("/login");
+    cy.get("[data-cy=button_email]").click();
+
+    cy.getInput("email")
+        .type(Cypress.env("userName"))
+        .should("have.value", Cypress.env("userName")); 
+    
+    cy.getInput("password")
+        .type(Cypress.env("password"))
+        .should("have.value", Cypress.env("password"));
+
+    cy.getButton("submit").click();
+    cy.location('pathname').should('match', /\/calendar\/day\/*$/);
+    cy.wait(5000);
+    cy.getWaitClick('[data-intercom-target="Sidebar-Reports"]', 0); 
+
+  })
+
+})
