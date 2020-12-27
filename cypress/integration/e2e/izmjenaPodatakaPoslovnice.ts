a6fb11acb45123563c8819e8aed479e76fcc69f2
FilipRukavina <fil.rukavina@gmail.com>
2020-12-27 17:58:29 +0100

    struktura testa, izmjena identifiera, wait

--- cypress/integration/e2e/izmjenaPodatakaPoslovnice.ts
+++ cypress/integration/e2e/izmjenaPodatakaPoslovnice.ts
@@ -10,7 +10,7 @@ export const module = 1;
 
 describe("Owner can login and edit branch data", () => {
 
-  it("Login", () => {
+  beforeEach("Login", () => {
     
     cy.visit("/login");
     cy.get("[data-cy=button_email]").click();
@@ -31,9 +31,31 @@ describe("Owner can login and edit branch data", () => {
 
   })
 
-  it("Navigate to branch data in settings", () => {
-    cy.getWaitClick(".dUbUBi@", 500); // identifier!
-    cy.getFirstWaitClick(".hyiaHA", 500); // identifier!
+  it("Change Branch data", () => {
+
+    cy.wait(3000);
+    cy.get('[data-intercom-target="Sidebar-Settings"]')
+      .click();
+    //cy.getWaitClick('[data-intercom-target="Sidebar-Settings"]', 500); 
+  
+  })
+
+  it("Confirm Branch data changes", () => {
+    
+    cy.wait(3000);
+    cy.get('[data-intercom-target="Sidebar-Settings"]')
+      .click();
+    //cy.getWaitClick('[data-intercom-target="Sidebar-Settings"]', 500); 
+  
+  })
+
+  it("Revert Branch data changes", () => {
+    
+    cy.wait(3000);
+    cy.get('[data-intercom-target="Sidebar-Settings"]')
+      .click();
+    //cy.getWaitClick('[data-intercom-target="Sidebar-Settings"]', 500); 
+  
   })
 
 })
